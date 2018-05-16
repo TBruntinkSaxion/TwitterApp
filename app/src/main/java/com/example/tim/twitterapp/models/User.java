75f0551133d66528117abff6e04f286103d57175
timscomputer\Tim <timbruntink@outlook.com>
2018-05-16 09:32:29 +0200

    Begin layout user gemaakt en begin user model gemaakt

---
+++ app/src/main/java/com/example/tim/twitterapp/models/User.java
@@ -0,0 +1,13 @@
+package com.example.tim.twitterapp.models;
+
+public class User {
+
+    private long id;
+    private String id_str;
+    private String name;
+    private String screen_name;
+    private String location;
+    private String description;
+    private String url;
+    private String profile_image_url;
+}
