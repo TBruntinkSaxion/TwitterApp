c88de5fdea9345514782f57ce7c7afcea989251d
timscomputer\Tim <timbruntink@outlook.com>
2018-05-16 10:37:18 +0200

    User layout voorzien dummy data

--- app/src/main/java/nl/saxion/tk/twitterapp/models/User.java
+++ app/src/main/java/nl/saxion/tk/twitterapp/models/User.java
@@ -10,4 +10,43 @@ public class User {
     private String description;
     private String url;
     private String profile_image_url;
+
+    public User(String name, String screen_name, String location, String description) {
+        this.name = name;
+        this.screen_name = screen_name;
+        this.location = location;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getScreen_name() {
+        return screen_name;
+    }
+
+    public void setScreen_name(String screen_name) {
+        this.screen_name = screen_name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- app/src/main/java/nl/saxion/tk/twitterapp/ui/UserActivity.java
+++ app/src/main/java/nl/saxion/tk/twitterapp/ui/UserActivity.java
@@ -2,12 +2,40 @@ package nl.saxion.tk.twitterapp.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import nl.saxion.tk.twitterapp.R;
+import nl.saxion.tk.twitterapp.models.User;
 
 public class UserActivity extends AppCompatActivity {
+
+    private User user;
+    private TextView textViewName, textViewScreenName, textViewDescription,textViewLocation,textViewFriendsCount,textViewFollowersCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(nl.saxion.tk.twitterapp.R.layout.activity_user);
+
+        textViewName = findViewById(R.id.textViewName);
+        textViewScreenName = findViewById(R.id.textViewScreenName);
+        textViewDescription = findViewById(R.id.textViewDescription);
+        textViewLocation = findViewById(R.id.textViewLocation);
+        textViewFriendsCount = findViewById(R.id.textViewFriendsCount);
+        textViewFollowersCount = findViewById(R.id.textViewFollowersCount);
+
+
+        //TODO dit vervangen door dynamische user info
+        this.user = new User("Hightech Twente","twente","Enschede","Twente Branding houdt zich bezig met de nationale en internationale positionering van Twente als innovatieve hightech kennisregio.");
+
+        loadUser();
+    }
+
+    private void loadUser(){
+
+        textViewName.setText(user.getName());
+        textViewScreenName.setText(user.getScreen_name());
+        textViewDescription.setText(user.getDescription());
+        textViewLocation.setText(user.getLocation());
     }
 }
